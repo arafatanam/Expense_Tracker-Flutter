bbd1a4d70da8b8c2e2eaffeaf08cee9244885ef3
arafatanam <arafatanam01@gmail.com>
2025-11-27 10:53:51 +0600

    Flutter UI Fixes.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -22,7 +22,7 @@ class _ExpensesState extends State<Expenses> {
     ),
     Expense(
       title: 'Cinema',
-      amount: 15.99,
+      amount: 15.49,
       date: DateTime.now(),
       category: Category.leisure,
     ),
