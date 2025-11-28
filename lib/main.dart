e89c86bd8d4b910a4148497e0beb87b71cf93464
arafatanam <arafatanam01@gmail.com>
2025-11-28 10:47:14 +0600

    Enhance UI by updating text style in ExpenseItem and adding dismiss background in ExpensesList

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ void main() {
           titleLarge: TextStyle(
             fontWeight: FontWeight.bold,
             color: kColorScheme.onSecondaryContainer,
-            fontSize: 14,
+            fontSize: 16,
           ),
         ),
       ),
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -18,6 +18,14 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index].id),
+        background: Container(
+          color: Theme.of(
+            context,
+          ).colorScheme.error.withAlpha((0.75 * 255).round()),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
