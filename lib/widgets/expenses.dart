b4400adc662d49e936627a537b2e91530297571d
arafatanam <arafatanam01@gmail.com>
2025-12-01 20:17:59 +0600

    Add Chart and ChartBar widgets to visualize expenses by category

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import '../models/expense.dart';
 import 'expenses_list/expenses_list.dart';
+import 'chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -89,7 +90,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The chart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
       ),
